48a09f808592c4d160efbfe06a9e54853f31fbab
glowlh <m.o.gushchina@gmail.com>
2016-06-16 02:04:15 +0300

    Modified header_box & footer_box. Installed gulp-source-glob.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -5,6 +5,7 @@ module.exports = function() {
     return $.gulp.src('./source/style/app.scss')
       .pipe($.gp.sourcemaps.init())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
+      .pipe($.gp.sassGlob())
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
       .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.config.root + '/assets/css'))
