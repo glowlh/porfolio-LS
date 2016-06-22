986a4c2a3203415fc8ac7fa1f32ef9b10355cd46
glowlh <m.o.gushchina@gmail.com>
2016-06-22 10:41:32 +0300

    Modified under header triangles on the pages 'about'&blog .

---
+++ source/js/modules/flipper.js
@@ -0,0 +1,20 @@
+'use strict';
+
+$(document).ready(function () {
+  $(".authorisation-button").on("click", function(event) {
+    $("#flipper").addClass("flipped");
+    $(".authorisation-button").addClass("hidden");
+    event.stopPropagation();
+  });
+  $(".background-welcome__video").on("click", function(event) {
+    $("#flipper").removeClass("flipped");
+    $(".authorisation-button").removeClass("hidden");
+    event.stopPropagation();
+  });
+  $(document).on("click", function(event) {
+    if ($(event.target).parents("#flipper").length == 0) {
+      $("#flipper").removeClass("flipped");
+      $(".authorisation-button").removeClass("hidden");
+    }
+  });
+});
