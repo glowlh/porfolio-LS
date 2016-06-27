dc759f4ab38fa3984173daf3edd5ff345489bc83
glowlh <m.o.gushchina@gmail.com>
2016-06-27 20:00:05 +0300

    Added moving blog menu navigation.

--- source/js/modules/blogNavigation.js
+++ source/js/modules/blogNavigation.js
@@ -1,6 +1,33 @@
 $(function () {
+    var menuOffsetTop = $("#blog-navigation").offset().top;
     $(document).scroll(function () {
-        // console.log($("#blog-navigation").offset().top);
-        console.log($(document).scrollTop());
+        var width = $("#blog-navigation").width();
+        if ($(document).scrollTop() >= menuOffsetTop){
+            $("#blog-navigation").css({
+                "position": "fixed",
+                "top"     : "0px",
+                "width"    : width
+            });
+        } else {
+            $("#blog-navigation").css({
+                "position": "static"
+            });
+        }
+        $(".content__article").each(function () {
+            if (($(document).scrollTop() - $(this).offset().top) >= 0){
+                $(".articles__item").each(function () {
+                    $(this).removeClass('articles__item--active');
+                });
+                var currentLink = $(".articles__link[href=\'#" + $(this).attr('id') + "\']");
+                currentLink.parent().addClass('articles__item--active');
+            }
+        });
+    });
+
+    $(".articles__link").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
     });
 });
