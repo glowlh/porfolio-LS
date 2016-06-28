868534b55662e71409aee18f086417473ad95471
glowlh <m.o.gushchina@gmail.com>
2016-06-28 16:11:12 +0300

    Added preloader. Updated blog navidation (added element inspection).

--- source/js/modules/blogNavigation.js
+++ source/js/modules/blogNavigation.js
@@ -1,29 +1,32 @@
 $(function () {
-    // 'use strict';
-    var menuOffsetTop = $("#blog-navigation").offset().top;
-    $(document).scroll(function () {
-        var width = $("#blog-navigation").width();
-        if ($(document).scrollTop() >= menuOffsetTop){
-            $("#blog-navigation").css({
-                "position": "fixed",
-                "top"     : "0px",
-                "width"    : width
-            });
-        } else {
-            $("#blog-navigation").css({
-                "position": "static"
-            });
-        }
-        $(".content__article").each(function () {
-            if (($(document).scrollTop() - $(this).offset().top) >= 0){
-                $(".articles__item").each(function () {
-                    $(this).removeClass('articles__item--active');
+    'use strict';
+    if($("#blog-navigation").length) {
+        var menuOffsetTop = $("#blog-navigation").offset().top;
+        $(document).scroll(function () {
+            var width = $("#blog-navigation").width();
+            if ($(document).scrollTop() >= menuOffsetTop){
+                $("#blog-navigation").css({
+                    "position": "fixed",
+                    "top"     : "0px",
+                    "width"    : width
                 });
-                var currentLink = $(".articles__link[href=\'#" + $(this).attr('id') + "\']");
-                currentLink.parent().addClass('articles__item--active');
+            } else {
+                console.log("here");
+                $("#blog-navigation").css({
+                    "position": "static"
+                });
             }
+            $(".content__article").each(function () {
+                if (($(document).scrollTop() - $(this).offset().top) >= 0){
+                    $(".articles__item").each(function () {
+                        $(this).removeClass('articles__item--active');
+                    });
+                    var currentLink = $(".articles__link[href=\'#" + $(this).attr('id') + "\']");
+                    currentLink.parent().addClass('articles__item--active');
+                }
+            });
         });
-    });
+    }
 
     $(".articles__link").on("click", function (event) {
         event.preventDefault();
---
+++ source/js/modules/preloader.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $(".preloader").css({
+        display: 'flex'
+    });
+    $(function(){
+        var media = [],
+            percentsImage = 1;
+        $.each($('*'), function(){
+            var that = $(this),
+                background = that.css('background-image'),
+                image = that.is('img'),
+                video = that.is('source');
+
+            if(background != 'none') {
+                var path = background.replace('url("', '').replace('")', '');
+                media.push(path);
+            }
+            if(image) {
+                var path = that.attr('src');
+                if (path) {
+                    media.push(path);
+                }
+            }
+            // if(video) {
+            //   var path = that.attr('src');
+            //     if(path) {
+            //         media.push(path);
+            //     }
+            // }
+        });
+
+        for (var i = 0; i < media.length; i++) {
+            var image = $('<img>', {
+                attr: {
+                    src : media[i]
+                }
+            });
+            image.load(function () {
+                setPercents(media.length, percentsImage);
+                percentsImage++;
+            });
+        }
+
+        var setPercents = function(total, current){
+            var percent = Math.ceil(current/total*100);
+            if(percent >= 100) {
+                $(".preloader").fadeOut('slow');
+            }
+            $(".preloader .big").css({
+                'stroke-dasharray': Math.ceil(percent*157/100) + 'px 157px'
+            });
+            $(".inner-text").text(percent);
+        }
+    });
+
+});
